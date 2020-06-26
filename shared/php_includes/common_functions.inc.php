ba7f33c2c89f0005758372dff3526e814c89fe98
Daniel Marschall <info@daniel-marschall.de>
2020-06-26 12:51:30 +0000

    Pattern update
    git-svn-id: https://svn.viathinksoft.com/svn/vgwhois/trunk@48 fc10f506-8e08-4f67-967e-a087fa965a98

--- shared/php_includes/common_functions.inc.php
+++ shared/php_includes/common_functions.inc.php
@@ -264,7 +264,7 @@ function generateRandomToken($haystack, $length = 20) {
 
 function github_commit_count($user, $repo) {
 	$cont = file_get_contents("https://github.com/$user/$repo");
-	if (preg_match('@>\s*(\d+)\s*</span>\s*commits@smU', $cont, $m)) {
+	if (preg_match('@<strong>(\\d+)</strong>\s*<span aria-label="Commits on master">commits</span>@smU', $cont, $m)) {
 	    return $m[1];
 	} else {
 		return "?";
